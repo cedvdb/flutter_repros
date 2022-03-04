f8315840134ee6a0f9fb4ed1b741a72be30c5e51
cedvdb <cedvandenbosch@gmail.com>
2022-03-05 00:17:34 +0100

    removed focus

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:phone_form_field/phone_form_field.dart';
+import 'package:pinput/pinput.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,11 +24,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BehaviorSubject<int> streamController = BehaviorSubject.seeded(0);
+
+  void _showOtherPage(BuildContext context) async {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => Scaffold(
+          appBar: AppBar(),
+          body: const Pinput(
+            autofocus: true,
+            showCursor: true,
+          ),
+        ),
+      ),
+    );
+    streamController.add(streamController.value + 1);
+    await Future.delayed(const Duration(milliseconds: 800));
+    streamController.add(streamController.value + 1);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: streamController.stream,
+        builder: (ctx, AsyncSnapshot<int> snap) {
+          if (!snap.hasData) {
+            return Container();
+          }
+          if (snap.data! % 2 == 0) {
+            return Column(
+              children: [
+                TextFormField(autofocus: true),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => _showOtherPage(context),
+                  child: const Text('show other page'),
+                ),
+              ],
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
+
+class FormView extends StatelessWidget {
+  const FormView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PhoneFormField(autofocus: true);
   }
 }
